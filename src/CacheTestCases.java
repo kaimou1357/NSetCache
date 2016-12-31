6f0e3d0ca248503c516aeb5467a36e6688391253
Kai Mou <kaimou1357@gmail.com>
2016-12-31 16:04:32 -0500

    Added test cases

--- src/CacheTestCases.java
+++ src/CacheTestCases.java
@@ -1,16 +1,64 @@
+import org.junit.Assert;
+import org.junit.Test;
+
 /**
  * Created by Kai Mou on 12/27/2016.
+ * Unit Testing class for the NSet Associative Cache for The Trade Desk
  */
 public class CacheTestCases {
     public static void main(String[] args){
-        ClientCacheSet test = new ClientCacheSet(2, "MRU");
-        ClientCache c = new ClientCache(2, 4, test);
-        c.put("HelenHu", "Hi");
-        c.put("Leeroy", "Dog");
-        System.out.println(c.get("HelenHu"));
-        System.out.println(c.get("Leeroy"));
-        c.put("Kai", "Hi should be evicted");
-        System.out.println(c.get("HelenHu"));
-        System.out.println(c.get("Leeroy"));
+        testBasicReadWrite();
+        testUpdateSameKey();
+        testLRU();
+        testMRU();
+        System.out.println("All test cases passed!");
     }
+    @Test
+    public static void testBasicReadWrite(){
+        ClientCache cache = new ClientCache(2, 8);
+        cache.put("Leeroy", "Jenkins");
+        cache.put("Counter", "Strike");
+        Assert.assertEquals(cache.get("Leeroy"), "Jenkins");
+        Assert.assertEquals(cache.get("Counter"), "Strike");
+
+    }
+
+    @Test
+    public static void testUpdateSameKey(){
+        ClientCache cache = new ClientCache(2, 8);
+        cache.put(16, "Kai");
+        cache.put(16, "Mou");
+        Assert.assertEquals(cache.get(16), "Mou");
+
+    }
+    @Test
+    public static void testLRU(){
+        ClientCache cache = new ClientCache(2, 2);
+        //create a 2 way cache with max of two entries. Should only be 1 set.
+        cache.put("Leeroy", "Jenkins");
+        cache.put("Kai", "Mou");
+
+        cache.put("Extra", "Hi");
+        //If LRU, "Jenkins" should be evicted.
+        Assert.assertNull(cache.get("Leeroy"));
+        Assert.assertEquals(cache.get("Kai"), "Mou");
+    }
+
+    @Test
+    public static void testMRU(){
+        ClientCacheSet set = new ClientCacheSet(2, "MRU");
+        ClientCache cache = new ClientCache(2,2, set);
+
+        cache.put("Leeroy", "Jenkins");
+        cache.put("Kai", "Mou");
+
+        cache.put("Extra", "Hi");
+        //if MRU, "Mou" should be evicted
+        Assert.assertNull(cache.get("Kai"));
+        Assert.assertEquals(cache.get("Leeroy"), "Jenkins");
+
+    }
+
+
+
 }
